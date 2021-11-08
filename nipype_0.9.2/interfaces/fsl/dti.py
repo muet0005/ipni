8930e9ae40d7033fe3ce876fd5598b07c70e71f3
Ryan Muetzel <muet0005@gmail.com>
2021-11-08 12:44:46 +0100

    fixed issue with cpbvecs in bedpostx cartesius call.  Also added wls option to core nipupe code.

--- nipype_0.9.2/interfaces/fsl/dti.py
+++ nipype_0.9.2/interfaces/fsl/dti.py
@@ -49,7 +49,8 @@ class DTIFitInputSpec(FSLCommandInputSpec):
     cni = File(exists=True, desc='input counfound regressors', argstr='-cni %s')
     little_bit = traits.Bool(desc='only process small area of brain',
                              argstr='--littlebit')
-
+    wls = traits.Bool(
+        desc='weighted least squares tensor fit', argstr='--wls')
 
 class DTIFitOutputSpec(TraitedSpec):
 
